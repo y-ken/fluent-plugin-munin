42cb928eeb0c6ac37b8a8d556334aea5ad105dd9
y-ken <y.ken.studio@gmail.com>
2012-10-04 14:10:57 +0900

    release v0.1.2

--- lib/fluent/plugin/in_munin.rb
+++ lib/fluent/plugin/in_munin.rb
@@ -17,11 +17,11 @@ module Fluent
     def configure(conf)
       super
       service_list = get_service_list
-      $log.info "munin-node #{@munin.nodes} provides #{service_list}"
+      $log.info "munin-node #{@munin.nodes.join(',')} provides #{service_list}"
       @interval = Config.time_value(@interval)
       @services = @service == 'all' ? service_list : @service.split(',')
       @record_hostname = @record_hostname || false
-      @hostname = @munin.nodes
+      @hostname = @munin.nodes.join(',')
     end
 
     def start
